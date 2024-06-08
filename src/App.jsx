b0f7e16b36f4fee013264b04008c6787d58f8bb8
brandon <webarebrandy@gmail.com>
2024-06-08 14:50:31 -0600

    fragments para anidar divs

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
+//importamos useState y Fragment de react para poder usarlos
+import { useState, Fragment } from "react";
 // aqui mandamos a llamar el lo que hay de estilos en app.css (estios locales)
-import { useState } from "react";
 import "./App.css"
 const App = () => {
-
   //estados useState() aqui recive el valor como array, objetos numeros,texto etc
   const [count, setCount]= useState(50);
 
@@ -22,7 +22,8 @@ const App = () => {
 
 
   return (
-    // container id "container" de donde se llamo desde index.css (gloables styles)) dentro de ese div
+    < Fragment>
+     {/* container id "container" de donde se llamo desde index.css (gloables styles)) dentro de ese div */}
     <div className="container">  
       <h1>Counter: {count} </h1>
       {/* hr es linea divisoria */}
@@ -31,6 +32,7 @@ const App = () => {
       <button onClick={handleSubtract}>-</button>
       <button onClick={handleReset}>reset</button>
     </div>
+    </Fragment>
   );
 };
 
