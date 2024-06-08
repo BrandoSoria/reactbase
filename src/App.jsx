3369782d81227f971c37fe20ad8f67441f09dad2
brandon <webarebrandy@gmail.com>
2024-06-08 00:30:57 -0600

    solo comente codigo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 const App = () => {
 
   return (
-    <div className="container">
+    // container id "container" de donde se llamo desde index.css dentro de ese div
+    <div className="container">  
       <h1>Hello World</h1>
     </div>
   );
