03871b43853e437dc8b578cc0bbdec75557bc195
brandon <webarebrandy@gmail.com>
2024-06-08 14:33:45 -0600

    eventos setState

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
+// aqui mandamos a llamar el lo que hay de estilos en app.css (estios locales)
+import { useState } from "react";
+import "./App.css"
 const App = () => {
 
+  //estados useState() aqui recive el valor como array, objetos numeros,texto etc
+  const [count, setCount]= useState(50);
+
+
   return (
-    // container id "container" de donde se llamo desde index.css dentro de ese div
+    // container id "container" de donde se llamo desde index.css (gloables styles)) dentro de ese div
     <div className="container">  
-      <h1>Hello World</h1>
+      <h1>Counter: {count} </h1>
+      {/* hr es linea divisoria */}
+      <hr />
+      <button>+</button>
+      <button>-</button>
+      <button>reset</button>
     </div>
   );
 };
